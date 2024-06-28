cb93b27f1ccba9875518a0b616dd0b40e180cabf
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-28 08:52:28 +0700

    update Attribute.php membaca attribute via reflection

--- Attribute.php
+++ Attribute.php
@@ -14,3 +14,13 @@
     #[NotBlank]
     var ?string $password;
   }
+
+  function validate(object $object): void
+  {
+    $class = new ReflectionClass($object);
+    $properties = $class->getProperties();
+    foreach($properties as $property)
+    {
+      validateNotBlank($property, $object);
+    }
+  }
