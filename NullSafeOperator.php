53b050a1f38ada4ad4dafa29adaf1ac06d2d9e1f
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-29 14:11:02 +0700

    update NullSafeOperator.php manual check null

--- NullSafeOperator.php
+++ NullSafeOperator.php
@@ -9,3 +9,18 @@
   {
     public ?Address $address;
   }
+
+  function getCountry(?User $user): ?string
+  {
+    if($user != null)
+    {
+      if($user->address != null)
+      {
+        return $user->address->country;
+      }
+    }
+
+    return null;
+  }
+
+  echo getCountry(null) . PHP_EOL;
