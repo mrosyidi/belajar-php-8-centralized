57337eec2e880d49288285d8cea0e244e1d2d392
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-29 10:29:07 +0700

    update MatchExpression.php dengan kondisi

--- MatchExpression.php
+++ MatchExpression.php
@@ -42,3 +42,12 @@
   };
 
   echo $result . PHP_EOL;
+
+  $name = "Mr. Eko";
+  $result = match(true){
+    str_contains($name, "Mr.") => "Hello Sir",
+    str_contains($name, "Mrs.") => "Hello Mam",
+    default => "Hello"
+  };
+
+  echo $result . PHP_EOL;
