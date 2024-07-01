80ee38f5372f16efa5b9520404f172a930a28d76
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-01 14:06:27 +0700

    add NewStringFunction.php

---
+++ NewStringFunction.php
@@ -0,0 +1,8 @@
+<?php
+
+  var_dump(str_contains("Eko Kurniawana", "Eko"));
+  var_dump(str_contains("Eko Kurniawana", "Kurniawana"));
+  var_dump(str_starts_with("Eko Kurniawana", "Eko"));
+  var_dump(str_starts_with("Eko Kurniawana", "Kurniawana"));
+  var_dump(str_ends_with("Eko Kurniawana", "Eko"));
+  var_dump(str_ends_with("Eko Kurniawana", "Kurniawana"));
